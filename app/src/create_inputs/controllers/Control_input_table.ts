e45309e864403a2d8437557b13b58322d0536e6b
Gabriel Lima <69268986+Rugaldromeda@users.noreply.github.com>
2022-08-16 18:17:27 -0300

    Criando mvc para sistema de inputs

---
+++ app/src/create_inputs/controllers/Control_input_table.ts
@@ -0,0 +1,10 @@
+"use strict"
+
+import { ListInputs } from "../models/ListInputs.js"
+
+export class ControlInputTable {
+    private listInputs: ListInputs;
+    private areaInputs: HTMLElement;
+    private areaTables: HTMLElement;
+
+}
---
+++ app/src/create_inputs/models/ListInputs.ts
@@ -0,0 +1,8 @@
+"use strict"
+
+import { List } from "../../abstract_class/list.js"
+import { ModelInput } from "./ModelInput.js"
+
+export class ListInputs extends List<ModelInput>{
+    
+}
